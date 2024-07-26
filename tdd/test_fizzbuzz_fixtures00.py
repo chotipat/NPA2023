ad581de31a034114d0c2e2b1f1aac386dee6e68e
Chotipat Pornavalai <chotipat@it.kmitl.ac.th>
2024-07-26 08:40:29 +0700

    add fixtures00 setup and teardown

--- tdd/test_fizzbuzz_fixtures00.py
+++ tdd/test_fizzbuzz_fixtures00.py
@@ -3,24 +3,42 @@ from fizzbuzz import fizzbuzz
 
 
 @pytest.fixture
-def test_input():
-    return list(range(1, 6))
+def setup_teardown():
+    print("\nSetup testing")
+    yield
+    print("\nClean testing")
 
 
-def test_fizzbuzz(test_input):
-    assert 1 == fizzbuzz(test_input[0])
-    assert 2 == fizzbuzz(test_input[1])
-    assert "Fizz" == fizzbuzz(test_input[2])
-    assert 4 == fizzbuzz(test_input[3])
-    assert "Buzz" == fizzbuzz(test_input[4])
-    assert "Fizz" == fizzbuzz(6)
+@pytest.mark.num
+def test_num(setup_teardown):
+    assert 1 == fizzbuzz(1)
+    assert 2 == fizzbuzz(2)
+    assert 4 == fizzbuzz(4)
     assert 7 == fizzbuzz(7)
-    assert "FizzBuzz" == fizzbuzz(15)
+
+
+@pytest.mark.fizz
+def test_fizz(setup_teardown):
+    assert "Fizz" == fizzbuzz(3)
+    assert "Fizz" == fizzbuzz(6)
     assert "Fizz" == fizzbuzz(18)
+
+
+@pytest.mark.buzz
+def test_buzz(setup_teardown):
+    assert "Buzz" == fizzbuzz(5)
     assert "Buzz" == fizzbuzz(20)
+
+
+@pytest.mark.fizzbuzz
+def test_fizzbuzz(setup_teardown):
+    assert "FizzBuzz" == fizzbuzz(15)
     assert "FizzBuzz" == fizzbuzz(30)
-    print("All tests are passed.")
 
 
 if __name__ == "__main__":
-    print("Please run with pytest")
+    test_num()
+    test_fizz()
+    test_buzz()
+    test_fizzbuzz()
+    print("All tests are passed.")
